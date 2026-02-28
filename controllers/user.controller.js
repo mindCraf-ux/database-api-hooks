a4239ed9b01871d8a6fafb5c013f677dde5d0720
mindCraf-ux <saurabhsharmauk03@gmail.com>
2026-02-28 14:12:04 +0530

    Add functionality to fetch user's watch history with video owner details & endpoints

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -416,8 +416,54 @@ const user = await User.aggregate([
     $match: {
       _id: new mongoose.types.ObjectId(req.user._id)
     }
+  },
+
+  {
+    $lookup:{
+      from: "videos",
+      localField: "watchHistory",
+      foreignField: "_id",
+      as: "watchHistory",
+
+      pipeline: [
+{
+  $lookup:{
+    from: "users",
+    localField: "owner",
+    foreignField: "_id",
+    as: "owner",
+    pipeline:[
+      {
+        $project:{
+          fullName: 1,
+          username: 1,
+          avatar: 1
+        }
+      }
+    ]
+  }
+},
+{
+  $addFields:{
+    owner:{
+      $first: "$owner"
+    }
+  }
+}
+      ]
+    }
   }
 ])
+
+return res 
+.status(200)
+.json(
+  new ApiResponse
+  (200,
+    user[0].watchHistory,
+    "Watch history fetched successfully"
+  )
+)
 })
 export {registerUser,
   loginUser,
